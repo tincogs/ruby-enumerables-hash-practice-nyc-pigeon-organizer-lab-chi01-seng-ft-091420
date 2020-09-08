fcaf23243b22128bfdc80201de47647e352870cc
Tom ODay <tomoday91@gmail.com>
2020-09-08 22:04:09 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+    pigeon_list = data.each_with_object({}) do |(key, value), new_hash|
+        value.each do |inner_key, names|
+            names.each do |name|
+                if !new_hash[name]
+                    new_hash[name] = {}
+                end
+                if !new_hash[name][key]
+                    new_hash[name][key] = []
+                end
+                new_hash[name][key].push(inner_key.to_s)
+            end
+        end
+    end
+    pigeon_list
 end
